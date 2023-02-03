1fdeb8450f42e095a0950a176fc604aa555846cd
hilu <nikanozadze1010@gmail.com>
2023-02-03 19:44:05 +0400

    feat: add theme flip function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+const moon = document.querySelector("#moon");
+const sun = document.querySelector("#sun");
+const title = document.querySelector(".title");
+const themeText = document.querySelector(".theme-text");
+
+const flipTheme = (theme) => {
+  if (theme === "dark") {
+    moon.style.display = "none";
+    sun.style.display = "block";
+    document.body.style.backgroundColor = "#141D2F";
+  } else {
+  }
+
+  title.classList.toggle("dark");
+  themeText.classList.toggle("dark");
+};
+
+moon.addEventListener("click", () => flipTheme("dark"));
