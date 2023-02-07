fbc354b1061504fe27e8b1865311ae246478fa30
hilu <nikanozadze1010@gmail.com>
2023-02-07 12:15:54 +0400

    wip: work with form

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 const title = document.querySelector(".title");
 const themeText = document.querySelector(".theme-text");
+const input = document.querySelector("#user");
+const button = document.querySelector(".btn");
 
 const flipTheme = (theme) => {
   if (theme === "dark") {
@@ -16,3 +18,7 @@ const flipTheme = (theme) => {
 };
 
 moon.addEventListener("click", () => flipTheme("dark"));
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+});
