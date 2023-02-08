290f61adaa5099f369a5911d597c37b5c68684a4
hilu <nikanozadze1010@gmail.com>
2023-02-08 13:31:20 +0400

    feat: add axios

--- script.js
+++ script.js
@@ -5,6 +5,41 @@ const themeText = document.querySelector(".theme-text");
 const input = document.querySelector("#user");
 const button = document.querySelector(".btn");
 
+const octocat = {
+  avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
+  bio: null,
+  blog: "https://github.blog",
+  company: "@github",
+  created_at: "2011-01-25T18:44:36Z",
+  email: null,
+  events_url: "https://api.github.com/users/octocat/events{/privacy}",
+  followers: 8291,
+  followers_url: "https://api.github.com/users/octocat/followers",
+  following: 9,
+  following_url: "https://api.github.com/users/octocat/following{/other_user}",
+  gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
+  gravatar_id: "",
+  hireable: null,
+  html_url: "https://github.com/octocat",
+  id: 583231,
+  location: "San Francisco",
+  login: "octocat",
+  name: "The Octocat",
+  node_id: "MDQ6VXNlcjU4MzIzMQ==",
+  organizations_url: "https://api.github.com/users/octocat/orgs",
+  public_gists: 8,
+  public_repos: 8,
+  received_events_url: "https://api.github.com/users/octocat/received_events",
+  repos_url: "https://api.github.com/users/octocat/repos",
+  site_admin: false,
+  starred_url: "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
+  twitter_username: null,
+  type: "User",
+  updated_at: "2023-01-22T12:13:51Z",
+  url: "https://api.github.com/users/octocat",
+};
+
 const flipTheme = (theme) => {
   if (theme === "dark") {
     moon.style.display = "none";
@@ -19,6 +54,16 @@ const flipTheme = (theme) => {
 
 moon.addEventListener("click", () => flipTheme("dark"));
 
-button.addEventListener("click", (event) => {
+button.addEventListener("click", async (event) => {
   event.preventDefault();
+  try {
+    const response = await axios.get(
+      "https://api.github.com/users/" + input.value
+    );
+    const user = response.data;
+    input.value = "";
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
